669fd615b70d7fd07ec12bfb3628ad65ea2ef3a9
Alexander-Tengborg <landervich@gmail.com>
2025-10-04 15:09:18 +0200

    Finished chapter 14 and the entire book, spheres are now placed randomly

--- src/main.cpp
+++ src/main.cpp
@@ -17,42 +17,68 @@ int main()
     // World
     HittableList world;
 
-    auto material_ground = std::make_shared<Lambertian>(Color(0.8, 0.3, 0.1));
-    auto material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-    auto material_left = std::make_shared<Dielectric>(1.50);
-    auto material_bubble = std::make_shared<Dielectric>(1.00 / 1.50);
-    auto material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
-    auto material_close_right = std::make_shared<Metal>(Color(0.2, 0.6, 0.2), 0.0);
+    auto ground_material = std::make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
+    world.add(std::make_shared<Sphere>(Point3(0, -1000, 0), 1000, ground_material));
 
-    world.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100, material_ground));
-    world.add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5, material_center));
-    world.add(std::make_shared<Sphere>(Point3(-0.9, -0.1, -0.8), 0.4, material_left));
-    world.add(std::make_shared<Sphere>(Point3(-0.9, -0.1, -0.8), 0.25, material_bubble));
-    world.add(std::make_shared<Sphere>(Point3(2.0, -0.2, -2), 0.3, material_right));
-    world.add(std::make_shared<Sphere>(Point3(1.2, -0.3, -1), 0.1, material_close_right));
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto choose_mat = randomDouble();
+            Point3 center(a + 0.9*randomDouble(), 0.2, b + 0.9*randomDouble());
 
-    // auto R = std::cos(PI/4);
+            if ((center - Point3(4, 0.2, 0)).length() > 0.9)
+            {
+                std::shared_ptr<Material> sphere_material;
 
-    // auto material_left = std::make_shared<Lambertian>(Color(0, 0, 1));
-    // auto material_right = std::make_shared<Lambertian>(Color(1, 0, 0));
+                if (choose_mat < 0.8)
+                {
+                    // Diffuse material
+                    auto albedo = Color::random() * Color::random();
+                    sphere_material = std::make_shared<Lambertian>(albedo);
 
-    // world.add(std::make_shared<Sphere>(Point3(-R, 0, -1), R, material_left));
-    // world.add(std::make_shared<Sphere>(Point3(R, 0, -1), R, material_right));
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // Metal material
+                    auto albedo = Color::random(0.5, 1);
+                    auto fuzz = randomDouble(0, 0.5);
+                    sphere_material = std::make_shared<Metal>(albedo, fuzz);
+                }
+                else
+                {
+                    // Glass material
+                    sphere_material = std::make_shared<Dielectric>(1.5);
+                }
+
+                world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
+            }
+        }
+    }
+
+    // auto material1 = std::make_shared<Dielectric>(1.5);
+    // world.add(std::make_shared<Sphere>(Point3(0, 1, 0), 1.0, material1));
+
+    // auto material2 = std::make_shared<Lambertian>(Color(0.4, 0.2, 0.1));
+    // world.add(std::make_shared<Sphere>(Point3(-4, 1, 0), 1.0, material2));
+
+    // auto material3 = std::make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.0);
+    // world.add(std::make_shared<Sphere>(Point3(4, 1, 0), 1.0, material3));
 
     // Camera
     Camera camera;
     camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 800;
-    camera.samples_per_pixel = 1000;
-    camera.max_depth = 500;
+    camera.image_width = 1920;
+    camera.samples_per_pixel = 500;
+    camera.max_depth = 200;
 
     camera.vfov = 20;
-    camera.look_from = Point3(-2, 2, 1);
-    camera.look_at = Point3(-0, 0, -1);
+    camera.look_from = Point3(13, 2, 3);
+    camera.look_at = Point3(0, 0,0);
     camera.v_up = Vec3(0, 1, 0);
 
-    // camera.defocus_angle = 10.0;
-    // camera.focus_dist = 3.4;
+    camera.defocus_angle = 0.6;
+    camera.focus_dist = 10.0;
 
     camera.render(world);
 
