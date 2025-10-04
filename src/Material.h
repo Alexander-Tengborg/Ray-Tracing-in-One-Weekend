e863a86d9e622147919eea1303a1a26466c377f6
Alexander-Tengborg <landervich@gmail.com>
2025-10-04 12:04:59 +0200

    Finished chapter 11, dielectrics

--- src/Material.h
+++ src/Material.h
@@ -69,3 +69,43 @@ private:
     Color albedo_;
     double fuzz_;
 };
+
+class Dielectric : public Material
+{
+public:
+    Dielectric(double refraction_index) : refraction_index_(refraction_index) {}
+
+    bool scatter(const Ray& incident_ray, const HitRecord& rec, Color& attenuation, Ray& scattered_ray) const override
+    {
+        attenuation = Color(1.0, 1.0, 1.0);
+        double ri = rec.front_face ? (1.0/refraction_index_) : refraction_index_;
+
+        auto unit_direction = unitVector(incident_ray.direction());
+        double cos_theta = std::fmin(dot(-incident_ray.direction(), rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1.0;
+        Vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, ri) > randomDouble())
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, ri);
+
+        scattered_ray = Ray(rec.p, direction);
+        return true;
+    }
+
+private:
+    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    double refraction_index_;
+
+    static double reflectance(double cos, double refraction_index)
+    {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = std::pow((1 - refraction_index) / (1 + refraction_index), 2);
+
+        return r0 + (1 - r0) * std::pow(1 - cos, 5);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,15 @@ int main()
 
     auto material_ground = std::make_shared<Lambertian>(Color(0.8, 0.3, 0.1));
     auto material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-    auto material_left = std::make_shared<Metal>(Color(0.8, 0.8, 0.8), 0.3);
+    auto material_left = std::make_shared<Dielectric>(1.50);
+    auto material_bubble = std::make_shared<Dielectric>(1.00 / 1.50);
     auto material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
     auto material_close_right = std::make_shared<Metal>(Color(0.2, 0.6, 0.2), 0.0);
 
     world.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100, material_ground));
     world.add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5, material_center));
-    world.add(std::make_shared<Sphere>(Point3(-2.0, -0.4, -2), 0.3, material_left));
+    world.add(std::make_shared<Sphere>(Point3(-0.9, -0.1, -0.8), 0.4, material_left));
+    world.add(std::make_shared<Sphere>(Point3(-0.9, -0.1, -0.8), 0.25, material_bubble));
     world.add(std::make_shared<Sphere>(Point3(2.0, -0.2, -2), 0.3, material_right));
     world.add(std::make_shared<Sphere>(Point3(1.2, -0.3, -1), 0.1, material_close_right));
 
@@ -33,8 +35,8 @@ int main()
     Camera camera;
     camera.aspect_ratio = 16.0 / 9.0;
     camera.image_width = 1280;
-    camera.samples_per_pixel = 100;
-    camera.max_depth = 50;
+    camera.samples_per_pixel = 1000;
+    camera.max_depth = 500;
 
     camera.render(world);
 
